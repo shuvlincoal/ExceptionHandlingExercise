f087fd9ab70f2b849f630788ea4d8487eafc26ff
mangosax <mcguerty@att.net>
2023-09-15 13:07:10 -0500

    first push

--- ExceptionHandlingExercise/Program.cs
+++ ExceptionHandlingExercise/Program.cs
@@ -9,40 +9,75 @@ namespace ExceptionHandlingExercise
         static void Main(string[] args)
         {
             // -----------------------------------------------------------------------------
-            // First create an char[], it must contain 6 numbers and 3 letters - name it arr
-            // Create a list called numbers that will hold integers
+            // First create an char[], it must contain 6 charList and 3 letters - name it arr
+            // Create a list called charList that will hold integers
             // Create an string variable with an empty string initializer - name it str
+            char[] charArray = new char[9] { '1', '2', '3', '4', '5', '6', 'a', 'b', 'c' };
+            List<int> charList = new List<int>();
+            string str = "";
+            int tmpInt = 0;
+
 
             // using a foreach loop, attempt to parse the elements in your char[] with int.Parse()
             // and Exceptions will be thrown 
             // Below we will set this up 
             // ------------------------------------------------------------------------------
+            foreach (var character in charArray)
+            {
+                try
+                {
+                    //str = character.ToString();
+                    //Console.WriteLine($"Char: {character}");
+                    int parsedNumber = int.Parse(character.ToString());
+                    charList.Add(parsedNumber);
+                }//end try
+                catch (Exception ex)
+                {
+                    //Console.WriteLine($"Bad things happened, unabled to read char: {character}");
+                    Console.WriteLine($"1st CATCH Unable to Parse '{character}'"); //character will be the name of each item in your collection
+                    //Console.WriteLine(ex.Message);
+                }//end catch
 
+            }//foreach
 
+            foreach (var alphaNumericChr in charList)
+            {
+                Console.WriteLine($"2nd FOREACH in FIRST TRY/CATCH Nbrs: {alphaNumericChr}");
 
-            //TODO START HERE:
-            
+            }
+            //TODO [PART 2] START HERE:
             // Make a foreach loop to iterate through your character array
             
-                // Now create a try catch
-                
-                
-                    // Inside your try block
-                        // set your string variable to each array element in your char[] to .ToString()
-                        // Now, using int.Parse, parse your string variable and store in an int variable
-                        // Then add each int to your list
-                
-                // catch your Exception:
-                // in the scope of your catch you can use the following, 
-                
-                    //Console.WriteLine($"Unable to Parse '{character}'"); //character will be the name of each item in your collection
-                
-            
+            // Now create a try catch
+            // Inside your try block
+            // set your string variable to each array element in your char[] to .ToString()
+            // Now, using int.Parse, parse your string variable and store in an int variable
+            // Then add each int to your list
 
-            //foreach (var num in numbers)
-            //{
-            //    Console.WriteLine(num);
-            //}
+            // catch your Exception:
+            // in the scope of your catch you can use the following, 
+
+            //2nd part
+            foreach (var chrInArrary in charArray)
+            {
+            try
+                {
+                   str = chrInArrary.ToString();
+                   Console.WriteLine($"2nd TRY BLOCK str: {str}");
+                   tmpInt = int.Parse(str);
+                   charList.Add(tmpInt);
+                }
+            catch
+                {
+                    Console.WriteLine($"2nd CATCH POO Happened ");
+                }
+            }
+            //Console.WriteLine($"Unable to Parse '{character}'"); //character will be the name of each item in your collection
+
+            foreach (var num in charList)
+            {
+                Console.WriteLine($"VERY LAST FOREACH Nbrs: {num}");
+            }
         }
     }
 }
